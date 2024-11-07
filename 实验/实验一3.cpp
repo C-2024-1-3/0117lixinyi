d9fe6b67b407eb9178373daaf2bd3f715da1abb1
Amamiya Lian <zhandui5463219@163.com>
2024-11-07 21:30:36 +0800

    Add files via upload

--- 实验/实验一3.cpp
+++ 实验/实验一3.cpp
@@ -4,4 +4,19 @@ int main()
 {
 	cout << "char length:" << sizeof(char) << endl;
 	cout << "int length:" << sizeof(int) << endl;
+	cout << "bool length:" << sizeof(bool) << endl;
+	cout << "short length:" << sizeof(short) << endl;
+	cout << "long length:" << sizeof(long) << endl;
+	cout << "float length:" << sizeof(float) << endl;
+	cout << "double length:" << sizeof(double) << endl;
+	cout << "long double length:" << sizeof(long double) << endl;
+	cout << "unsigned char length:" << sizeof(unsigned char) << endl;
+	cout << " unsigned int length:" << sizeof(unsigned int) << endl;
+	cout << "unsigned short length:" << sizeof(unsigned short) << endl;
+	cout << "unsigned long length:" << sizeof(unsigned long) << endl;
+	cout << "void length:" << sizeof(void) << endl;
+	
+
+
+
 }
